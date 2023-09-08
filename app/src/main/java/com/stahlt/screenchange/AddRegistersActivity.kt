9c247039d5de76618cc17b22a8ef8ec8a2d56126
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 11:16:45 -0300

    feat: add new activity

---
+++ app/src/main/java/com/stahlt/screenchange/AddRegistersActivity.kt
@@ -0,0 +1,11 @@
+package com.stahlt.screenchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRegistersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_registers)
+    }
+}
