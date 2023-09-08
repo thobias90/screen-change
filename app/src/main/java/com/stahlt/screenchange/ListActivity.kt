819dbe94d2f8cbe9520bd679dea9e50580a73cdd
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 15:09:47 -0300

    feat: add result to intent return

--- app/src/main/java/com/stahlt/screenchange/ListActivity.kt
+++ app/src/main/java/com/stahlt/screenchange/ListActivity.kt
@@ -1,11 +1,27 @@
 package com.stahlt.screenchange
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import android.widget.Toast
 
 class ListActivity : AppCompatActivity() {
+    private lateinit var lvProducts: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        lvProducts = findViewById(R.id.lvProducts)
+        lvProducts.setOnItemClickListener { adapterView, view, pos, l ->
+            Toast.makeText(this, "Position: $pos", Toast.LENGTH_SHORT).show()
+            val code = pos.inc().toString()
+            Intent().apply {
+                putExtra("productCode", code)
+                setResult(RESULT_OK, this)
+            }
+            finish()
+        }
     }
 }
