6984b383fefb52c649c956c996847443d6af012b
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 11:30:45 -0300

    feat: add confirm register activity

---
+++ app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
@@ -0,0 +1,11 @@
+package com.stahlt.screenchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterConfirmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_confirm)
+    }
+}
