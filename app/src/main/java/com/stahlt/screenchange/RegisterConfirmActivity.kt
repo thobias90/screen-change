4f9f8605dbac95c9857f9a4d4c7661cb0fb8897c
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 11:44:56 -0300

    feat: add confirm register activity widgets

--- app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
+++ app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
@@ -2,10 +2,13 @@ package com.stahlt.screenchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegisterConfirmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_confirm)
     }
+
+    fun btConfirmOnClick(view: View) {}
 }
