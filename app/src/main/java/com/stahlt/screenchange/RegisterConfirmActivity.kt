14123537d34a6a4428602f9cc692d2356f6851d2
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 12:03:57 -0300

    feat: retrieve shared data to show on text view widgets

--- app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
+++ app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
@@ -3,11 +3,28 @@ package com.stahlt.screenchange
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class RegisterConfirmActivity : AppCompatActivity() {
+    private lateinit var tvConfirmCode: TextView
+    private lateinit var tvConfirmQuantity: TextView
+    private lateinit var tvConfirmPrice: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_confirm)
+
+        tvConfirmCode = findViewById(R.id.tvConfirmCode)
+        tvConfirmQuantity = findViewById(R.id.tvConfirmQuantity)
+        tvConfirmPrice = findViewById(R.id.tvConfirmPrice)
+
+        val code = intent.getStringExtra("code")
+        val quantity = intent.getStringExtra("quantity")
+        val price = intent.getStringExtra("price")
+
+        tvConfirmCode.text = code
+        tvConfirmQuantity.text = quantity
+        tvConfirmPrice.text = price
     }
 
     fun btConfirmOnClick(view: View) {}
