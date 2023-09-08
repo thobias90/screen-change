a4e3f1e069642b1f9d935296de4386713cce8d1a
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 14:21:24 -0300

    feat: add confirm logic to send sms with data

--- app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
+++ app/src/main/java/com/stahlt/screenchange/RegisterConfirmActivity.kt
@@ -1,10 +1,15 @@
 package com.stahlt.screenchange
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import java.net.URI
 
 class RegisterConfirmActivity : AppCompatActivity() {
     private lateinit var tvConfirmCode: TextView
@@ -27,5 +32,27 @@ class RegisterConfirmActivity : AppCompatActivity() {
         tvConfirmPrice.text = price
     }
 
-    fun btConfirmOnClick(view: View) {}
+    fun btConfirmOnClick(view: View) {
+        val sms = "Code: ${tvConfirmCode.text.toString()}, Quantity: ${tvConfirmQuantity.text.toString()} , Price: ${tvConfirmPrice.text.toString()}"
+
+        val uri = Uri.parse("smsto:+5546991056342")
+        val intent = Intent(Intent.ACTION_SENDTO, uri)
+        intent.putExtra("address", "+554691056342");
+        intent.putExtra("sms_body", sms);
+
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT -> {
+                //Getting the default sms app.
+                val defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(this)
+
+                // Can be null in case that there is no default, then the user would be able to choose
+                // any app that support this intent.
+                if (defaultSmsPackageName != null) intent.setPackage(defaultSmsPackageName)
+
+                startActivity(intent)
+            }
+            else -> startActivity(intent)
+        }
+
+    }
 }
