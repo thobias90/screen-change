0652fe85368562f13e08f980d526cfb390245c1c
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 11:25:36 -0300

    refactor: add on click callback for add register activity

--- app/src/main/java/com/stahlt/screenchange/AddRegistersActivity.kt
+++ app/src/main/java/com/stahlt/screenchange/AddRegistersActivity.kt
@@ -2,10 +2,14 @@ package com.stahlt.screenchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AddRegistersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_registers)
     }
+
+    fun btClearOnClick(view: View) {}
+    fun btAddOnClick(view: View) {}
 }
